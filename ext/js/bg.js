0d7632f0b26a6832d1a0740ceab8ce968c95a749
vibedivide <x@sys32.dev>
2021-04-09 09:03:17 -0400

    finalize extension

--- ext/js/bg.js
+++ ext/js/bg.js
@@ -168,7 +168,7 @@ var a=document.createElement('script');a.innerHTML=${bundler.wrap('document.curr
 	setInterval(bundle, sploit.tick);
 });
 
-if(alert('The sploit extension is discontinued, do you wish to install the tampermonkey script?')){
+if(confirm('The sploit extension is discontinued, do you wish to install the tampermonkey script?')){
 	chrome.tabs.create({ url: 'https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo' });
 	chrome.tabs.create({ url: 'https://e9x.github.io/kru/#userscript' });
 	
