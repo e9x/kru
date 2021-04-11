66acce3481fe28f3e1e5572d6d16a1cf39be315a
vibedivide <x@sys32.dev>
2021-04-11 12:25:30 -0400

    make auto update check on start

--- src/update.js
+++ src/update.js
@@ -1,19 +1,20 @@
-var latest_script = 'https://raw.githubusercontent.com/e9x/kru/master/sploit.user.js';
+var latest_script = 'https://raw.githubusercontent.com/e9x/kru/master/sploit.user.js',
+	check_update = async () => {
+		if(build_extracted >= new Date(parse_headers(await fetch(latest_script).then(res => res.text())).extracted).getTime())return;
+		
+		clearInterval(update_interval);
+		
+		if(!confirm('A new sploit version is available, do you wish to update?'))return;
+		
+		window.open(latest_script);
+	},
+	parse_headers = script => {
+		var out = {};
 
-var parse_headers = script => {
-	var out = {};
+		script.replace(/\/\/ ==UserScript==\n([\s\S]*?)\n\/\/ ==\/UserScript==/, (match, headers) => headers.split('\n').forEach(line => line.replace(/@(\S+)\s+(.*)/, (match, label, value) => out[label] = label in out ? [].concat(out[label], value) : value)));
 
-	script.replace(/\/\/ ==UserScript==\n([\s\S]*?)\n\/\/ ==\/UserScript==/, (match, headers) => headers.split('\n').forEach(line => line.replace(/@(\S+)\s+(.*)/, (match, label, value) => out[label] = label in out ? [].concat(out[label], value) : value)));
+		return out;
+	},
+	update_interval = setInterval(check_update, 1000 * 60 * 5); // 5 minutes
 
-	return out;
-};
-
-var update_interval = setInterval(async () => {
-	if(build_extracted >= new Date(parse_headers(await fetch(latest_script).then(res => res.text())).extracted).getTime())return;
-	
-	clearInterval(update_interval);
-	
-	if(!confirm('A new sploit version is available, do you wish to update?'))return;
-	
-	window.open(latest_script);
-}, 1000 * 60 * 5); // 5 minutes
+check_update();
