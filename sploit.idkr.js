c14cf2a34bda10bf17a7721c21a651b1018e91a4
vibedivide <x@sys32.dev>
2021-04-11 12:08:07 -0400

    Update sploit.idkr.js

--- sploit.idkr.js
+++ sploit.idkr.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: 'Sploit',
-    author: 'Divide',
-    locations: ['game'],
-    run(){
+	name: 'Sploit',
+	author: 'Divide',
+	locations: ['game'],
+	run(){
 		var request = new XMLHttpRequest();
 		request.open('GET', 'https://raw.githubusercontent.com/e9x/kru/master/sploit.user.js', false);
 		request.send();
