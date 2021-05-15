641414ff57612254725477ba8c202c1d6ad26301
vibedivide <x@sys32.dev>
2021-05-15 00:31:49 -0400

    wip changes in building

--- index.js
+++ index.js
@@ -10,15 +10,24 @@ var os = require('os'),
 		
 		return compiler = webpack({
 			entry: script.entry,
-			output: { path: path.dirname(script.output), filename: path.basename(script.output) },
-			module: { rules: [ { test: /\.css$/, use: [ { loader: path.join(__dirname, 'src', 'css.js'), options: {} } ] } ] },
-			optimization: { minimize: minimize  },
+			output: {
+				path: path.dirname(script.output),
+				filename: path.basename(script.output),
+			},
+			context: path.dirname(script.entry),
+			module: { rules: [
+				{ test: /\.css$/, use: [ { loader: path.join(__dirname, 'src', 'css.js') } ] },
+				{ test: /\.json$/, use: [ { loader: path.join(__dirname, 'json.js') } ], type: 'javascript/auto' },
+			] },
+			mode: minimize ? 'production' : 'development',
+			devtool: false,
+			// optimization: { minimize: minimize },
 			plugins: [
 				{ apply: compiler => compiler.hooks.thisCompilation.tap('Replace', compilation => compilation.hooks.processAssets.tap({ name: 'Replace', stage: webpack.Compilation.PROCESS_ASSETS_STAGE_REPORT }, () => {
 					var file = compilation.getAsset(compiler.options.output.filename),
 						spackage = script.package(),
 						extracted = new Date(),
-						rmeta = {
+						rmeta = Object.assign({
 							name: spackage.name,
 							author: spackage.author,
 							source: 'https://github.com/e9x/kru',
@@ -30,9 +39,8 @@ var os = require('os'),
 							extracted: extracted.toGMTString(),
 							include: /^https?:\/\/(internal\.|comp\.)?(krunker\.io|browserfps\.com)\/*?(index.html)?(\?|$)/,
 							'run-at': 'document-start',
-							grant: script.no_grants ? 'none' : [ 'GM_setValue', 'GM_getValue', 'GM_xmlhttpRequest' ],
 							connect: [ 'sys32.dev', 'githubusercontent.com' ],
-						},
+						}, script.meta),
 						meta = Object.entries(rmeta).flatMap(([ key, val ]) => Array.isArray(val) ? val.map(val => [ key, val ]) : [ [ key, val ] ]),
 						whitespace = meta.map(meta => meta[0]).sort((a, b) => b.toString().length - a.toString().length)[0].length + 8,
 						source = file.source.source().replace(/build_extracted/g, extracted.getTime());
@@ -42,7 +50,6 @@ var os = require('os'),
 					compilation.updateAsset(file.name, new webpack.sources.RawSource(`// ==UserScript==
 ${meta.map(([ key, val ]) => ('// @' + key).padEnd(whitespace, ' ') + val.toString()).join('\n')}
 // ==/UserScript==
-// For any concerns regarding minified code, you are encouraged to build from the source
 ${(script.after || []).join('\n')}
 ${source}`));
 				})) },
@@ -65,7 +72,7 @@ ${source}`));
 create_script({
 	package(){
 		return {
-			name: 'Junker',
+			name: 'Krunker Junker',
 			author: 'SkidLamer',
 			version: '1.0',
 			description: 'Junk in Your Krunk Guaranteed',
@@ -75,8 +82,12 @@ create_script({
 			repository: { type: 'git', url: 'git+https://github.com/e9x/kru.git' },
 		};
 	},
+	meta: {
+		icon: 'https://i.imgur.com/pA5e8hy.png',
+		grant: 'none',
+	},
 	after: [
-		`// For license information please see https://raw.githubusercontent.com/e9x/kru/master/junker.user.js.LICENSE.txt`,
+		`// For license information, please see https://raw.githubusercontent.com/e9x/kru/master/junker.user.js.LICENSE.txt`,
 		``,
 		`// Donations Accepted`,
 		`// BTC:  3CsDVq96KgmyPjktUe1YgVSurJVe7LT53G`,
@@ -88,7 +99,6 @@ create_script({
 	// greasyfork
 	minify: false,
 	// window is another context when grants are given, never noticed this in sploit
-	no_grants: true,
 	entry: path.join(__dirname, 'junker', 'index.js'),
 	output: path.join(__dirname, 'junker.user.js'),
 });
@@ -98,9 +108,14 @@ create_script({
 		return JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
 	},
 	after:[
-		`// For license information please see https://raw.githubusercontent.com/e9x/kru/master/sploit.user.js.LICENSE.txt`,
+		`// For license information, please see https://raw.githubusercontent.com/e9x/kru/master/sploit.user.js.LICENSE.txt`,
 		``,
 	],
+	meta: {
+		grant: [ 'GM_setValue', 'GM_getValue', 'GM_xmlhttpRequest' ],
+	},
+	// greasyfork
+	minify: false,
 	entry: path.join(__dirname, 'src', 'index.js'),
 	output: path.join(__dirname, 'sploit.user.js'),
 });
