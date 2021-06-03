416cd217361ffdf28302d596b6e15c5b97bd3aeb
vibedivide <x@sys32.dev>
2021-06-03 15:43:50 -0400

    remove extension icon, preload stuff

--- client/preload.js
+++ client/preload.js
@@ -1,43 +1,10 @@
 'use strict';
 
-var run = 'junker';
-
 var fs = require('fs'),
 	path = require('path'),
-	mod = require('module'),
-	contextify = process.binding('contextify'),
-	wrequire = (func, base, loaders = {}, cache = {}, req) => req = Object.assign(fn => {
-		var resolved = req.resolve(fn);
-		
-		if(!fs.existsSync(resolved))return require(resolved);
-		
-		if(req.cache[resolved])return req.cache[resolved].exports;
-		
-		var mod = req.cache[resolved] = Object.setPrototypeOf({ exports: Object.setPrototypeOf({}, null), filename: resolved, id: resolved, path: path.dirname(resolved), loaded: true, children: [] }, Module.prototype),
-			ext = path.extname(resolved),
-			args = {
-				module: mod,
-				exports: mod.exports,
-				require: wrequire(func, mod.path, loaders, cache),
-				global: window,
-				__dirname: mod.path,
-				__filename: resolved,
-			},
-			script = req.loaders[ext] ? req.loaders[ext](fs.readFileSync(resolved)) : fs.readFileSync(resolved) + '\n//@ sourceURL=' + resolved;
-		
-		contextify.compileFunction(script, resolved, 0, 0, undefined, false, undefined, [], Object.keys(args)).function.apply(mod.exports, Object.values(args));
-		
-		return mod.exports;
-	}, {
-		resolve: fn => req.base.resolve(fn),
-		base: mod.createRequire(base + '/'),
-		cache: cache,
-		loaders: loaders,
-	});
+	Module = require('module'),
+	main = path.join(__dirname, '..', 'sploit');
 
-class Module {}
+Module._extensions['.css'] = (module, filename) => module.exports = fs.readFileSync(filename, 'utf8').replace(/(["'])assets:(.*?)\1/g, (match, quote, string) => JSON.stringify('data:application/octet-stream;base64,' + fs.readFileSync(path.join(main, 'libs', string), 'base64')));
 
-setImmediate(wrequire(window.Function, path.join(__dirname, '..', run), {
-	'.json': require(path.join(__dirname, '..', 'sploit', 'libs', 'json.js')),
-	'.css': require(path.join(__dirname, '..', 'sploit', 'libs', 'css.js')),
-}, {}).bind({}, '.'));
+require(main);
